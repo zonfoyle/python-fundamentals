702cc5271df3da4e7f5ac19765e553b0586298d5
Zonique Foyle <zoniquefoyle@Zoniques-Mac-mini.local>
2025-12-30 22:45:11 -0800

    Add 30 Days of Python (without data) + Day 1 practice

---
+++ 30_days_of_python/01_Day_Introduction/day_01_practice.py
@@ -0,0 +1,25 @@
+# Day 1 Practice - Zonique 
+# 30 Days of Python 
+
+# Arithmetic Operations
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 // 2)
+
+# Data Types
+print(type(10))                # Integer
+print(type(3.14))              # Float
+print(type("Zonique"))         # Str
+print(type([1, 2, 3]))         # List
+print(type({'name': 'Zonique'})) # Dictionary
+
+#Variables
+name = "Zonique"
+goal = "Solutions Engineer/Architect"
+Year = 2025
+
+print(name, goal, Year)
