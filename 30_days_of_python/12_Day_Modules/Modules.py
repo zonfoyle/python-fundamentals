8e8a780a93e51ebdcf7244652201786a276126c9
Zonique Foyle <zonique9@gmail.com>
2026-01-25 14:07:43 -0800

    Added Day 12 - Python Modules notes and examples

--- 30_days_of_python/12_Day_Modules/Modules.py
+++ 30_days_of_python/12_Day_Modules/Modules.py
@@ -1,5 +1,63 @@
-#Math Modules 
+#Creating a Module
+# mymodule.py file
+def generate_full_name(firstname, lastname):
+    return firstname + ' ' + lastname
 
+#Importing a Module
+# main.py file
+import mymodule
+print(mymodule.generate_full_name('Zonique', 'Foyle')) # Zonique Foyle
+
+#Import Functions from a Module
+# main.py file
+from mymodule import generate_full_name, sum_two_nums, person, gravity
+print(generate_full_name('Zonique','Foyle'))    # Zonique Foyle
+print(sum_two_nums(1,9))
+mass = 100
+weight = mass * gravity
+print(weight)
+print(person['firstname'])
+
+#import Functions from a Module and Rename It
+# main.py file
+# main.py file
+from mymodule import generate_full_name as fullname, sum_two_nums as total, person as p, gravity as g
+print(fullname('Zonique','Foyle'))
+print(total(1, 9))
+mass = 100 
+weight = mass * g
+print(weight)
+print(p)
+print(p['firstname'])
+
+#Import Bulit-in Modules
+# OS Module
+# import the module
+import os
+# Creating a directory
+os.mkdir('directory_name')
+# Changing the current directory
+os.chdir('path')
+# Getting current working directory
+os.getcwd()
+# Removing directory
+os.rmdir()
+
+#Sys Module
+import sys
+#print(sys.argv[0], argv[1],sys.argv[2])  # this line would print out: filename argument1 argument2
+print('Welcome {}. Enjoy  {} challenge!'.format(sys.argv[1], sys.argv[2]))
+
+
+#Statistics Module
+from statistics import * # importing all the statistics modules
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))       # ~22.9
+print(median(ages))     # 23
+print(mode(ages))       # 20
+print(stdev(ages))      # ~2.3
+
+#Math Modules 
 import math 
 math.sqrt(16) #Find the square root of 16 # Returns 4.0 
 
@@ -16,6 +74,18 @@ math.log10(1000) #Returns the base-10 logarithm of 1000 # Returns 3.0
 math.floor(2.3) #Rounds down to the nearest integer # Returns 2
 
 math.ceil(2.3) #Rounds up to the nearest integer # Returns 3
+
+#String Module
+import string
+print(string.ascii_letters) # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(string.digits)        # 0123456789
+print(string.punctuation)   # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+#random Module
+from random import random, randint
+print(random())   # it doesn't take any arguments; it returns a value between 0 and 0.9999
+print(randint(5, 20)) # it returns a random integer number between [5, 20] inclusive
+
 
 
 #Calendar Module
